bda13f60ae5294f1cea15c3987e0c00136fa1cbb
JOHN ALAFIATAYO <81050461+splashray@users.noreply.github.com>
2022-11-01 05:57:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,13 @@ const server = http.createServer((req, res) => {
     } else {
         if (req.url === '/') {
            res.end(`
-           {
-           
-           "slackUsername": "Splashray",
+           {"slackUsername": "Splashray",
            "age": 22,
            "backend": true ,
            "bio": "I am a software Developer,
            passionate about building software that solves daily human problems using strong analytical, critical-thinking skills,
            and technological strategies. I'm also a fast learner and growth enthusiast and I believe communication and collaboration 
-           in any team are essential to achieving a common goal."
-           
-           }
+           in any team are essential to achieving a common goal"}
            `)
         }
     }
