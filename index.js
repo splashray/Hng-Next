7e49214d06aa8bb510d601d70534f9aaf2c27dff
JOHN ALAFIATAYO <81050461+splashray@users.noreply.github.com>
2022-11-01 05:36:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.setHeader("content-type","application/json")
     if (req.method !== 'GET') {
-        res.end(`{"error": "error in routes"}`)
+        res.end(`{"Message": "Rotues doesnt exist"}`)
     } else {
         if (req.url === '/') {
            res.end(`
-           {"slackUsername": "Splashray",
+           {
+           
+           "slackUsername": "Splashray",
            "age": 22,
            "backend": true ,
            "bio": "I am a software Developer,
            passionate about building software that solves daily human problems using strong analytical, critical-thinking skills,
            and technological strategies. I'm also a fast learner and growth enthusiast and I believe communication and collaboration 
-           in any team are essential to achieving a common goal."}
+           in any team are essential to achieving a common goal."
+           
+           }
            `)
         }
     }
